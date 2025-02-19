1671533ab26d51342015a72e32d104aa6929c13d
fakesib <fakesibbb@gmail.com>
2025-02-19 11:26:58 +0300

    Add arrays dir

---
+++ arrays/src/main/java/org/fakesibwork/task1/Main.java
@@ -0,0 +1,9 @@
+package org.fakesibwork.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] array = {1, 2, 3, 4};
+        solution.runningSum(array);
+    }
+}
