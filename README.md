83138265a21d0fb98ec5b65832fe6b30d9126cc9
fakesib <fakesibbb@gmail.com>
2025-02-25 12:22:26 +0300

    Delete all

--- README.md
+++
@@ -1,1 +0,0 @@
-Here is my leetCode tasks
--- arrays/src/main/java/org/fakesibwork/task2/Main.java
+++
@@ -1,9 +0,0 @@
-package org.fakesibwork.task2;
-
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] account = {{1, 2, 3}, {3, 2, 1}};
-        System.out.println(solution.maximumWealth(account));
-    }
-}
