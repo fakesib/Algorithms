fe390773d923d025905a23c5800ccb597dc4db7d
fakesib <fakesibbb@gmail.com>
2025-02-27 17:27:43 +0300

    Fixed binary algorithm

---
+++ java/binary/src/main/java/org/fakesib/BinarySearch.java
@@ -0,0 +1,20 @@
+package org.fakesib;
+
+public class BinarySearch {
+    int binarySearch(int[] list, int item){
+        int low = 0;
+        int high = list.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = list[mid];
+            if (guess == item) {
+                return mid;
+            } else if (guess >= item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return 0;
+    }
+}
---
+++ java/binary/src/main/java/org/fakesib/Main.java
@@ -0,0 +1,10 @@
+package org.fakesib;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println(binarySearch.binarySearch(array, 13));
+
+    }
+}
